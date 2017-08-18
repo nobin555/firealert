d07910a7baaab19adadf7c29e7cecb9dc3b0c941
riddler <cvdrops@outlook.com>
2017-08-18 11:24:54 +0530

    Notification

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var app = express()
 var request = require('request')
 var bodyParser = require('body-parser')
 
+app.use(express.static('public'))
+
 app.use(bodyParser.urlencoded({
     extended: false
 }))
