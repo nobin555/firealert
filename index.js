28b5ef8f104da140dd47ce3cefcf810b749ec1d8
riddler <cvdrops@outlook.com>
2017-08-18 12:03:05 +0530

    Map

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var app = express()
 var request = require('request')
 var bodyParser = require('body-parser')
 
+app.set('view engine', 'ejs');
 app.use(express.static('public'))
 
 app.use(bodyParser.urlencoded({
@@ -29,6 +30,18 @@ app.post('/alert', function (req, res) {
 
 });
 
+
+
+app.get('/location/:lng/:lat', function (req, res) {
+
+
+    res.render('location.ejs', {
+        lat: req.params.lat,
+        lng: req.params.lng
+    })
+
+
+})
 
 app.get('/', function (req, res) {
 
